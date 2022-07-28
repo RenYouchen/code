7b2a16550544e0d530e9f7f90e1c43b1606b73ea
RenYouchen <REN3442520390@163.com>
2022-07-28 13:21:46 +0800

    First Commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,88 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+    /**
+     * This function is run when the robot is first started up and should be used for any
+     * initialization code.
+     */
+
+    private static final Joystick joystick = new Joystick(0);
+
+    private final int ButtonA = 1;
+    private final int ButtonB = 2;
+    private final int ButtonX = 3;
+    private final int ButtonY = 4;
+
+    private final int ButtonLB = 5;
+    private final int ButtonRB = 6;
+
+    private final int ButtonBACK = 7;
+    private final int ButtonSTART = 8;
+
+    private double LT,RT;
+
+    private CANSparkMax motor_Chassis_LF = new CANSparkMax(1, MotorType.kBrushed);
+
+    @Override
+    public void robotInit() {
+    }
+
+    @Override
+    public void robotPeriodic() {
+    }
+
+    @Override
+    public void autonomousInit() {
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+    }
+
+    @Override
+    public void teleopInit() {
+    }
+
+    @Override
+    public void teleopPeriodic() {
+    }
+
+    @Override
+    public void disabledInit() {
+    }
+
+    @Override
+    public void disabledPeriodic() {
+    }
+
+    @Override
+    public void testInit() {
+    }
+
+    @Override
+    public void testPeriodic() {
+    }
+
+    @Override
+    public void simulationInit() {
+    }
+
+    @Override
+    public void simulationPeriodic() {
+    }
+}
